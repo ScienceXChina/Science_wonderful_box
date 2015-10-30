17a110af44670521adc34ee496d05c78f6a67733
Science <565369157@qq.com>
2015-10-30 13:12:52 +0000

    Add sources.list and repair the installation problem for Vim

--- Vim_Config_box/Vim_necessary_plugin.sh
+++ Vim_Config_box/Vim_necessary_plugin.sh
@@ -6,7 +6,22 @@ GRN='\E[1;32m'
 YEL='\E[1;33m'
 RES='\E[0m'
 
-#Dnowload Pathogen--->
+# HINT
+SUCCESS="${GRN} [ SUCCESS ]: ${RES}"
+WARNING="${YEL} [ WARNING ]: ${RES}"
+ERROR="${RED} [ ERROR ]: ${RES}"
+NEED_SOLVE="------------> "
+
+# Replace sources.list
+sudo cp -f ./sources.list /etc/apt/sources.list && sudo apt-get update
+grep -q "main restricted universe multiverse" /etc/apt/sources.list
+if [ $? -ne 0 ];then
+	echo -e "${ERROR} Please check the /etc/apt/sources.list"
+else
+	echo -e "${SUCCESS} --> REPLACED sources.list"
+fi
+
+# Dnowload Pathogen--->
 git clone https://github.com/tpope/vim-pathogen
 if [ ! -d ./vim-pathogen ];then 
 	while [ ! -d ./vim-pathogen ]
@@ -23,12 +38,12 @@ else
 			sudo mv ./vim-pathogen/autoload ~/.vim/
 			sudo rm -rf ./vim-pathogen
 		else
-			echo -e "${YEL} [ WARNING ]: ${RES} Please check the ~/.vim/ (May be Vim-pathogen had exist)."
+			echo -e "${WARNING} Please check the ~/.vim/ (May be Vim-pathogen had exist)."
 			sudo rm -rf ./vim-pathogen
 		fi
 	fi
 fi
-echo -e "${GRN} --> INSTALLED Vim-pathogen. ${RES}"
+echo -e "${SUCCESS} --> INSTALLED Vim-pathogen."
 
 #sudo sh -c 'echo "call pathogen#infect()
 #syntax on
@@ -37,18 +52,39 @@ echo -e "${GRN} --> INSTALLED Vim-pathogen. ${RES}"
 if [ ! -d ~/.vim/bundle ];then
 	mkdir ~/.vim/bundle
 else
-	echo -e "${YEL} [ WARNING ]: ${RES} ~/.vim/bundle had exist."
+	echo -e "${WARNING} ~/.vim/bundle had exist."
 fi
 
-#Dnowload Bundle (Vundle)
+# Dnowload Bundle (Vundle)
 if [ ! -d ~/.vim/bundle/Vundle.vim ];then
 	sudo git clone https://github.com/VundleVim/Vundle.vim.git 
 	sudo mv ./Vundle.vim ~/.vim/bundle/
 else
-	echo -e "${YEL} [ WARNING ]: ${RES} /.vim/bundle/Vundle.vim had exist."
+	echo -e "${WARNING} /.vim/bundle/Vundle.vim had exist."
 fi
 
-#Dnowload Ctags 
-sudo apt-get install ctags
-echo -e "${GRN} --> INSTALLED Ctags ${RES}"
+# Dnowload Ctags 
+for ((i=1; i<=2; i++));do
+	sudo apt-get install ctags > ./.record_install 
+	grep -q "0 upgraded, 0 newly installed" ./.record_install
+done
+grep -q "0 upgraded, 0 newly installed" ./.record_install
+if [ $? -ne 0 ];then
+	echo -e "${WARNING} Fail to install ctags"
+else
+	echo -e "${SUCCESS} --> INSTALLED Ctags"
+fi
+sudo rm -f ./.record_install
 
+# Dnowload Cscope
+for ((i=1; i<=2; i++));do
+	sudo apt-get install cscope > ./.record_install 
+	grep -q "0 upgraded, 0 newly installed" ./.record_install
+done
+grep -q "0 upgraded, 0 newly installed" ./.record_install
+if [ $? -ne 0 ];then
+	echo -e "${WARNING} Fail to install cscope"
+else
+	echo -e "${SUCCESS} --> INSTALLED Cscope"
+fi
+sudo rm -f ./.record_install
