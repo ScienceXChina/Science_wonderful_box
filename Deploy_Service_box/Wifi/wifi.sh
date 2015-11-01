81cab7288cd76b7a3dfb5a24528b6eda6e3a2f1e
Science <565369157@qq.com>
2015-11-01 09:25:34 +0000

    Repair a lot of problems for wifi.sh

--- Deploy_Service_box/Wifi/wifi.sh
+++ Deploy_Service_box/Wifi/wifi.sh
@@ -13,55 +13,64 @@ ERROR="${RED} [ ERROR ]: ${RES}"
 NEED_SOLVE="------------> "
 
 # VARIABLES
-REBOOT_PARAMETER=0
+
+# Reboot flags
+grep -q "REBOOT_PARAMETER=1" /etc/profile
+if [ $? -ne 0 ]; then
+	REBOOT_PARAMETER=0
+else
+	REBOOT_PARAMETER=1
+fi
 
 # Install Inter 7260 mini PCIe wireless driver for TK1 
 function INSTALL_FIRMWARE {
-	sudo apt-get install linux-firmware | grep "0 upgraded, 0 newly installed" > ./.record_install
+	sudo apt-get install linux-firmware | sudo tee ./.record_install
 }
 
 sudo apt-get update
 INSTALL_FIRMWARE
-grep -q "1 upgraded, 0 newly installed" ./.record_install
+grep -q "0 upgraded, 1 newly installed" ./.record_install
 while [ $? -ne 0 ];
 do
 	INSTALL_FIRMWARE
 	grep -q "0 upgraded, 0 newly installed" ./.record_install
 done
-sudo sh -c 'echo "export REBOOT_PARAMETER=1" >> /etc/profile'
-sudo sh -c 'echo "./$PWD/wifi.sh" >> /etc/profile'
-if [ $REBOOT_PARAMETER -eq 1 ];then
-	RESULT=$(grep -Fc "" /etc/profile)
-	let "RESULT_HEAD = $RESULT - 2 "
-	sudo sed -e "${RESULT_HEAD},${RESULT}d" /etc/profile
+# sudo sh -c 'echo "$PWD/wifi.sh" >> /etc/profile'
+if [ $REBOOT_PARAMETER -eq 1 ]; then
+	sudo mv /etc/profile.bak /etc/profile
 else
-	sudo reboot
+	sudo cp -f /etc/profile /etc/profile.bak
+	sudo sh -c 'echo "export REBOOT_PARAMETER=1" >> /etc/profile'
+	#sudo reboout
+	#echo "sleep 3 second"
 	#sudo sleep 3   #test the script
+	echo -e "${WARNING} Please reboot your system now and keep on ./wifi.sh ."
+	exit 1
 fi
 sudo rm -f ./.record_install
 
 # Check the Inter 7260 mini PCIe wireless driver for TK1
 lspci | grep -q "Network controller: Intel Corporation Wireless 7260"
 if [ $? -eq 0 ];then
-	echo -e "${SUCCESS}The driver of wireless is ok."
+	echo -e "${SUCCESS} The driver of wireless is ok."
 else
-	echo -e "${ERROR}The operation of wireless is unsuccessful."
-	echo -e "${NEED_SOLVE}${RED}Please reinstall the driver of wireless.${RES}"
+	echo -e "${ERROR} The operation of wireless is unsuccessful."
+	echo -e "${NEED_SOLVE}${RED} Please reinstall the driver of wireless.${RES}"
 	exit 1
 fi
 
 # Check the operation of wireless 
 ifconfig | grep -q "wlan0"
 if [ $? -eq 0 ];then
-	echo -e "${SUCCESS}The operation of wireless is ok."
+	echo -e "${SUCCESS} The operation of wireless is ok."
 else
-	echo -e "${WARNING}May be you should check it again."
-	echo -e "${NEED_SOLVE}${RED}Restart the wireless service.${RES}"
+	echo -e "${WARNING} May be you should check it again."
+	echo -e "${NEED_SOLVE}${RED} Restart the wireless service.${RES}"
 	exit 1
 fi
 
 # Find the available wireless and choose
-iwlist wlan0 scan | grep "ESSID:"
+sudo iwlist wlan0 scan | grep "ESSID:"
 read -p "Please choose wifi :" ESSID
 read -p "Please input password :" PSK
 
@@ -75,11 +84,11 @@ touch ./.wpa_supplicant.conf
 echo -e "network={
 	ssid=\"$ESSID\"
 	psk=\"$PSK\"
-	prioriity=5
+	priority=1
 }" > ./.wpa_supplicant.conf
 sudo sh -c 'cat ./.wpa_supplicant.conf >> /etc/wpa_supplicant.conf'
 sudo rm -f ./.wpa_supplicant.conf
-echo -e "${SUCCESS}Choose ${ESSID} is ok."
+echo -e "${SUCCESS} Choose ${ESSID} is ok."
 
 # Wifi interfaces service
 sudo sh -c 'echo "auto lo
@@ -89,12 +98,15 @@ iface eth0 inet dhcp
 allow-hotplug wlan0
 iface wlan0 inet manual
 wpa-roam /etc/wpa_supplicant.conf
-iface default inet dhcp" >> /etc/network/interfaces.test'
-echo -e "${SUCCESS}Deployed the wifi interfaces"
+iface default inet dhcp" >> /etc/network/interfaces'
+echo -e "${SUCCESS} Deployed the wifi interfaces"
 
 # The network service restart
-echo "Restart the network service now."
-sudo /etc/init.d/networking restart
+echo -e "${WARNING} Reboot for restarting the network service now."
+#sudo /etc/init.d/networking restart
+echo -e "${WARNING} if IP is not true,please execute the commond 
+\n --> sudo ifdown wlan0 && sudo ifup wlan0"
+
 
 
 
